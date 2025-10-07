a035540b1e7282af1c9f373aa8ecc222ebce817f
Chemish21 <smorris3131@gmail.com>
2025-10-06 23:24:26 -0400

    Update args.py
    updating argument parser for better usage experience

--- args.py
+++ args.py
@@ -1,15 +1,20 @@
 #Python
 import argparse
 
-#Implementing top level parsers
-parser = argparse.ArgumentParser()
-subparsers = parser.add_subparsers(dest="command")
+#Top-level parser
+parser = argparse.ArgumentParser(description="Get GitHub user activity")
 
-#Implementing subparser
-name_parser = subparsers.add_parser("name")
+#Positional argument for username
+parser.add_argument("username", type=str, help="GitHub username")
 
-#username parser
-name_parser.add_argument("username", type=str, help="enter username of github account")
+#Optional flags
+parser.add_argument("-t", "--total", action="store_true", help="Get total action count")
+parser.add_argument("-p", "--push", action="store_true", help="Get all recent pushes")
+parser.add_argument("-pl", "--pull", action="store_true", help="Get all recent pull requests")
+parser.add_argument("-s", "--star", action="store_true", help="Get all recent stars")
+parser.add_argument("-cr", "--create", action="store_true", help="Get all recent creates")
+parser.add_argument("-co", "--comment", action="store_true", help="Get all recent comments")
+parser.add_argument("-f", "--fork", action="store_true", help="Get all recent forks")
 
-#Finalizing parser
+#Parse arguments
 args = parser.parse_args()
