24c2560b038872489406a7714cd7b0903ede13d7
Chemish21 <smorris3131@gmail.com>
2025-10-06 23:22:28 -0400

    Update gitsee.py
    updating main to represent full functionality

--- gitsee.py
+++ gitsee.py
@@ -3,9 +3,25 @@ from args import args
 import tasks
 
 def main():
-  if args.command == 'name':
+    
     tasks.get_data(args.username)
-    tasks.find_events(args.username)
+
+    if args.total:
+        tasks.find_total(args.username)
+    elif args.push:
+        tasks.find_push(args.username)
+    elif args.pull:
+        tasks.find_pull(args.username)
+    elif args.star:
+        tasks.find_star(args.username)
+    elif args.create:
+        tasks.find_create(args.username)
+    elif args.comment:
+        tasks.find_comment(args.username)
+    elif args.fork:
+        tasks.find_fork(args.username)
+    else:
+        print("No valid option selected. Use -t, -p, -pl, -s, -cr, -co, or -f")
 
 if __name__ == '__main__':
-  main()
+    main()
