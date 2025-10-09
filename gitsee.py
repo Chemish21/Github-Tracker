6b67fa0ded1224da40bae6d996fe74cab8b814b5
Chemish21 <smorris3131@gmail.com>
2025-10-09 12:18:20 -0400

    Update gitsee.py
    adding comments for clarity

--- gitsee.py
+++ gitsee.py
@@ -3,17 +3,20 @@ from args import args
 import tasks
 
 def main():
-    
+    #Check to see if account data is already possessed
     if tasks.file_exist() == True:
+      #Confirming if username matches current stored data
       the_file_name = tasks.get_file_name()
       confirmed = tasks.confirm_data(args.username, the_file_name)
+      #If data matches move to tasks, else, need to get/update data
       if confirmed == True:
           pass
       else:
           tasks.get_data(args.username)
     else:
       tasks.get_data(args.username)
-
+      
+    #Tasks run based on option used
     if args.total:
         tasks.find_total(args.username)
     elif args.push:
